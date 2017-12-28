3f190bb98942a87741d25713b6a33d9e4adbe6ca
MUL-16-PC\my <513gun@naver.com>
2017-12-28 16:51:17 +0900

    회원 탈퇴 추가

--- FirebaseChat/app/src/main/java/futurewiz/cou/kr/firebasechat/etc/SettingActivity.java
+++ FirebaseChat/app/src/main/java/futurewiz/cou/kr/firebasechat/etc/SettingActivity.java
@@ -1,18 +1,24 @@
 package futurewiz.cou.kr.firebasechat.etc;
 
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
 import butterknife.BindView;
+import butterknife.OnCheckedChanged;
 import butterknife.OnClick;
 import futurewiz.cou.kr.firebasechat.R;
 import futurewiz.cou.kr.firebasechat.base.BaseActivity;
 
+import android.util.Log;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.Toast;
 import android.widget.Switch;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.ValueEventListener;
@@ -37,16 +43,6 @@ public class SettingActivity extends BaseActivity {
         setContentView(R.layout.activity_setting);
         super.onCreate(savedInstanceState);
 
-        allim.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-                Map<String, Object> childUpdates = new HashMap<>();
-                childUpdates.put("users/" + authManager.getFirebaseUser().getUid() + "/settings/push", b);
-
-                databaseReference.updateChildren(childUpdates);
-            }
-        });
-
         setTitle("설정");
     }
 
@@ -54,6 +50,10 @@ public class SettingActivity extends BaseActivity {
     protected void onResume() {
         super.onResume();
 
+        checkAllimSetting();
+    }
+
+    public void checkAllimSetting() {
         databaseReference.child("users/" + authManager.getFirebaseUser().getUid() + "/settings/push").addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
@@ -68,9 +68,33 @@ public class SettingActivity extends BaseActivity {
         });
     }
 
+    @OnCheckedChanged(R.id.allim)
+    public void onAllimCheckedChange(CompoundButton compoundButton, boolean b) {
+        Map<String, Object> childUpdates = new HashMap<>();
+        childUpdates.put("users/" + authManager.getFirebaseUser().getUid() + "/settings/push", b);
+
+        databaseReference.updateChildren(childUpdates);
+    }
+
     @OnClick(R.id.sign_out)
     public void onSignOutClick() {
         authManager.signOut();
         finish();
     }
+
+    @OnClick(R.id.delete)
+    public void onUserDeleteClick(){
+        final String firebaseUid = authManager.getFirebaseUser().getUid();
+
+        authManager.getFirebaseUser().delete().addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful()) {
+                    databaseReference.child("users/" + firebaseUid).removeValue();
+                    finish();
+                }
+            }
+        });
+    }
+
 }

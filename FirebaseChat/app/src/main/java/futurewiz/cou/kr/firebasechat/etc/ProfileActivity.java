5d5af752a243e2d0be2803f1bcb6d3194dc6caf1
qna116 <qna116@naver.com>
2018-01-21 18:02:30 +0900

    프로필 작업 (닉네임 변경)

--- FirebaseChat/app/src/main/java/futurewiz/cou/kr/firebasechat/etc/ProfileActivity.java
+++ FirebaseChat/app/src/main/java/futurewiz/cou/kr/firebasechat/etc/ProfileActivity.java
@@ -1,10 +1,24 @@
 package futurewiz.cou.kr.firebasechat.etc;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.ValueEventListener;
+
+import butterknife.BindView;
+import butterknife.OnClick;
 import futurewiz.cou.kr.firebasechat.R;
 import futurewiz.cou.kr.firebasechat.base.BaseActivity;
+import futurewiz.cou.kr.firebasechat.login.UserData;
 
 /**
  * Created by bag-yongtae on 2017. 12. 15..
@@ -13,11 +27,51 @@ import futurewiz.cou.kr.firebasechat.base.BaseActivity;
 
 public class ProfileActivity extends BaseActivity {
 
+    @BindView(R.id.email)
+    TextView email;
+
+    @BindView(R.id.editText)
+    EditText nickName;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         setContentView(R.layout.activity_profile);
         super.onCreate(savedInstanceState);
 
         setTitle("프로필");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        databaseReference.child("users/").child(authManager.getFirebaseUser().getUid()).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                UserData userData = dataSnapshot.getValue(UserData.class);
+                email.setText(userData.getEmail());
+                nickName.setText(userData.getName());
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    @OnClick(R.id.change_nickname)
+    public void onChangeNickNameClick() {
+        databaseReference.child("users/").child(authManager.getFirebaseUser().getUid()).child("name").setValue(nickName.getText().toString()).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful()) {
+                    authManager.getUserData().setName(nickName.getText().toString());
+                    Toast.makeText(ProfileActivity.this, "닉네임 변경 완료", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(ProfileActivity.this, "닉네임 변경 실패", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 }
